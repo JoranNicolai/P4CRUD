3412960a7035985f35f0fb6d2a68de5e101ebaa5
JoranNicolai <0196823@student.roc-nijmegen.nl>
2022-05-12 09:56:41 +0200

    kdAf
    t4

---
+++ CRUDP4-main/Websitepages/tickettabel/hotels.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../CSS/extra/hotels.css">
+</head>
+<body>
+
+<header class="header">
+    <img class="img" src="../../Images/logocrud.png" width="200px">
+    <div class="headersearch">
+        <a href="../index.php">Home</a>
+        <a href="../vluchten.php">Vluchten</a>
+        <a href="">Over ons</a>
+        <a href="">Contact</a>
+        <a href="">Login</a>
+    </div>
+</header>
+<header class="headerblokjes">
+    <div class="wrap">
+        <div class="search">
+            <input type="search" class="searchTerm" placeholder="Search...">
+
+            <i class="fa fa-search"></i>
+            </button>
+        </div>
+    </div>
+</header>
+<footer>
+    <div class="blok3">
+        <a href="../index.php"><h3>Vliegtickets</h3></a>
+    </div>
+    <div class="blok4">
+        <a href="bundels.php"><h3>Vlucht en hotel</h3></a>
+    </div>
+    <div class="blok5">
+        <a href="hotels.php"><h3>Hotels</h3></a>
+    </div>
+    <div class="blok6">
+        <a href="taxi.php"><h3>Taxi's</h3></a>
+    </div>
+    <div class="blok7">
+        <a href="verhuur.php"><h3>Auto verhuur</h3></a>
+    </div>
+    <div class="blok2">
+        <form class="formpje" action="">
+            <input class="formpje" type="text" placeholder="Waar begint uw vlucht?">
+        </form>
+        <form class="formpje" action="">
+            <input class="formpje" type="text" placeholder="Waar gaat uw vlucht heen?">
+        </form>
+        <form class="formpje1" action="">
+            <input class="formpje10" type="date" placeholder="Heen">
+        </form>
+        <form class="formpje1" action="">
+            <input class="formpje10" type="date" placeholder="Heen">
+        </form>
+        <form class="formpje4" action="">
+            <input class="formpje" type="number" placeholder="Aantal personen">
+        </form>
+        <input class="sumbit" type="submit" value="Boek je vlucht">
+    </div>
+
+    <div class="blok1">
+
+    </div>
+</body>
+</html>
