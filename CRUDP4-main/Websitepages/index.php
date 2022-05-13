60563bd838bb62184d83ae037ec84e4c4d3c5245
JoranNicolai <0196823@student.roc-nijmegen.nl>
2022-05-13 08:48:37 +0200

    commit
    commit 13.5.2022

--- CRUDP4-main/Websitepages/index.php
+++ CRUDP4-main/Websitepages/index.php
@@ -15,7 +15,7 @@
     <div class="headersearch">
         <a href="index.php">Home</a>
         <a href="vluchten.php">Vluchten</a>
-        <a href="">Over ons</a>
+        <a href="overons.php">Over ons</a>
         <a href="">Contact</a>
         <a href="">Login</a>
     </div>
