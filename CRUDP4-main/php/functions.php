e80de0c914b08ddfb4b058887c59cdea5ad96b4a
u138852p132552 <xday420@gmail.com>
2022-05-16 13:22:50 +0200

    admin

--- CRUDP4-main/php/functions.php
+++ CRUDP4-main/php/functions.php
@@ -88,7 +88,7 @@ function registerAcc(){
 // ...
 function isAdmin()
 {
-	if (isset($_SESSION['user']) && $_SESSION['user']['username'] == 'test' ) {
+	if (isset($_SESSION['user']) && $_SESSION['user']['username'] == 'admin' ) {
 		return true;
 	}else{
 		return false;
@@ -98,7 +98,7 @@ function isAdmin()
 // return user array from their id
 function getUserById($id){
 	global $db;
-	$query = "SELECT * FROM users WHERE Accountid=" . $id;
+	$query = "SELECT * FROM users WHERE id=" . $id;
 	$result = mysqli_query($db, $query);
 
 	$user = mysqli_fetch_assoc($result);
