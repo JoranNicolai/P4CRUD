3809b4cbad5d43dfc9b2d088ea0131116dabf7a0
JoranNicolai <0196823@student.roc-nijmegen.nl>
2022-05-23 13:01:48 +0200

    agfag
    awergfwg

--- CRUDP4-main/Websitepages/index.php
+++ CRUDP4-main/Websitepages/index.php
@@ -24,7 +24,7 @@
         <a href="index.php">Home</a>
         <a href="vluchten.php">Vluchten</a>
         <a href="overons.php">Over ons</a>
-        <a href="">Contact</ a>
+        <a href="contact.php">Contact</ a>
         <a href="./account/login.php">Login</a>
     </div>
 
