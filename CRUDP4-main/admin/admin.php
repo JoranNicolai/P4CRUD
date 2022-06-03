7150366186b904c445841b0b7c69f202848e29df
JoranNicolai <0196823@student.roc-nijmegen.nl>
2022-06-03 10:01:09 +0200

    4ttetet
    atwt3

--- CRUDP4-main/admin/admin.php
+++ CRUDP4-main/admin/admin.php
@@ -15,13 +15,16 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <video class="absulute" src="video/production ID_3678380.mp4"></video>
 </body>
+
 </html>
