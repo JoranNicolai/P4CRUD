cfaecd78c8213ddd353fa18a30c5400a4a53921d
JoranNicolai <0196823@student.roc-nijmegen.nl>
2022-06-16 09:37:55 +0200

    sds
    dsdd

--- CRUDP4-main/Websitepages/tickettabel/hotels.php
+++ CRUDP4-main/Websitepages/tickettabel/hotels.php
@@ -23,8 +23,9 @@
         <div class="headersearch">
             <a href="../index.php">Home</a>
             <a href="../vluchten.php">Vluchten</a>
+            
             <a href="../overons.php">Over ons</a>
-
+            <a href="../reviews.php">Reviews</a>
             <a href="../contact.php">Contact</a>
             <?php
         if (!isLoggedIn()) {
